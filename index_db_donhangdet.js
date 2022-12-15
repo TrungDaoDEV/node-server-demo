9a337ff109c03c2a2e4883d8263bb061bc05bb72
TrungDaoDEV <trungdao.dev@gmail.com>
2022-12-15 08:27:04 +0700

    update new info database

--- index_db_donhangdet.js
+++ index_db_donhangdet.js
@@ -127,10 +127,10 @@ var db;
 function handleDisconnect() {
     db = mysql.createConnection({
         host: 'us-cdbr-east-06.cleardb.net',
-        user: 'b1a67cdd6b2463',
-        password: '2ff783a8',
+        user: 'b87feaf6bc86ec',
+        password: '3623dace',
         // port: 8080,
-        database: 'heroku_567fe36f1fa15f6',
+        database: 'heroku_a342647ec0ec5bf',
     }); // Recreate the connection, since
     // the old one cannot be reused.
 
@@ -323,7 +323,8 @@ app.get('/loadmaychay', (req, res) => {
 	console.log("today: " + today);
 	console.log("yesterday: " + yesterday);
 
-    var sql = `SELECT *,maydet.Trangthai,maydet.May as May,maydet.TG_OFF as time_off, round(sum(ttmjson.TG_OFF/60),2) as stop, round(sum(ttmjson.TG_ON/60),2) as run `
+    var sql = `SELECT *,maydet.Trangthai,maydet.May as May,maydet.TG_OFF as time_off, round(sum(ttmjson.TG_OFF/60),2) as stop,`
+		+ ` round(sum(ttmjson.TG_ON/60),2) as run, sum(ttmjson.SL) as TSL `
         //+ ' FROM maydet LEFT JOIN ttmjson ON maydet.May=ttmjson.May and (ttmjson.Ngay BETWEEN ("2022-11-24 9:00:00") AND (NOW())) '
 		+ ` FROM maydet LEFT JOIN ttmjson ON maydet.May=ttmjson.May and (ttmjson.Ngay > "${time}") `
         + ` GROUP BY maydet.May`;

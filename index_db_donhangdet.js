bbacfef519f22d23db3856a67fb3a5a18d3eb94d
TrungDaoDEV <trungdao.dev@gmail.com>
2022-12-17 13:00:16 +0700

    đa fix duoc loadvochay

--- index_db_donhangdet.js
+++ index_db_donhangdet.js
@@ -188,14 +188,24 @@ app.get('/datamaydung', (req, res) => {
         res.send(result);//goi kq cho react native
     })
 })
-app.get('/loadloi', (req, res) => {
-    var sql = 'SELECT * FROM `maydet` WHERE Trangthai LIKE 0';
+app.get('/loadvochay', (req, res) => {
+	var test = '2022-11-23';
+    var sql = 'SELECT hanghoa.TenHH, sum(chitietdet.SL_Ngay) as TongNgay, sum(chitietdet.SL_TC) as TongTC, '
+        + 'sum(chitietdet.SL_Dem) as TongDem, sum(chitietdet.SL_Ngay+chitietdet.SL_TC+chitietdet.SL_Dem) as Total '
+        + 'FROM (hanghoa INNER JOIN chitietdonhang ON hanghoa.idHH=chitietdonhang.idHH) '
+       // + `INNER JOIN chitietdet ON (chitietdonhang.idCTDH=chitietdet.idCTDH AND (NgayDet > ${req.headers.tungay})) `
+       + `INNER JOIN chitietdet ON (chitietdonhang.idCTDH=chitietdet.idCTDH AND (NgayDet > ${test})) ` + "' "
+        // + 'INNER JOIN chitietdet ON (chitietdonhang.idCTDH=chitietdet.idCTDH AND (NgayDet > 2022-11-23)) '
+        + 'GROUP BY hanghoa.TenHH '
+        + 'ORDER BY hanghoa.TenHH';
+
+    // db.query(sql, data, (err, result) => {
     db.query(sql, (err, result) => {
         if (err) {
             console.log(err);
             throw err;
         }
-        console.log(" tai sao lai khong upload len dc server");
+        console.log("get /load vo chay");
         res.send(result);//goi kq cho react native
     })
 })
